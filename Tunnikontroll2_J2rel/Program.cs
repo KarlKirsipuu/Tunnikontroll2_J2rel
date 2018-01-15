82cefb1806ad7ff546caa1f0fc1b3889eb635485
Karl Kirsipuu <karl_kirsipuu@hot.ee>
2018-01-15 11:26:03 +0200

    töö

--- Tunnikontroll2_J2rel/Program.cs
+++ Tunnikontroll2_J2rel/Program.cs
@@ -10,6 +10,40 @@ namespace Tunnikontroll2_J2rel
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Sisesta erinevaid sõnu, lõpetamiseks sisest -1 ");
+
+            string input = " ";
+
+            List<string> sõnad = new List<string>();
+
+
+            while (input != "-1")
+            {
+                Console.Write("Sisesta sõna: ");
+                input = Console.ReadLine();
+                if (input != "-1")
+                {
+                    sõnad.Add(input);
+                }
+                
+            }
+
+            Console.WriteLine("Sisetatud sõnad: ");
+        
+            Console.WriteLine(String.Join(" ,", sõnad));
+
+            Console.WriteLine("Valin nendest välja suvalise sõna: ");
+            Random rnd = new Random();
+            int suvalineSõna = rnd.Next(0, sõnad.Count);
+            Console.Write(sõnad[suvalineSõna]);
+
+            
+            Console.ReadKey();
+
+
+
+           
+            
         }
     }
 }
