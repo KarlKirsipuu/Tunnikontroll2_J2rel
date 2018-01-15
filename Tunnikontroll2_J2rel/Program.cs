903a47036809494aadf3f3a4b9cc1434067a675c
Karl Kirsipuu <karl_kirsipuu@hot.ee>
2018-01-15 10:00:35 +0200

    Add project files.

---
+++ Tunnikontroll2_J2rel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tunnikontroll2_J2rel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
